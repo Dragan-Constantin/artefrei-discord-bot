38e5fd58f56967777262227e84e5c69bf18240d7
Dragan Constantin <constantin.dragan@efrei.net>
2024-10-01 15:25:29 +0200

    Create test.js

---
+++ commands/test.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'test',
+    description: 'Just a test command for the auto-update and reload of the bot.',
+    execute(message) {
+      message.channel.send('Mouahaha, this was a test and you... oh wait... oh... well then f**k you.');
+    }
+  };
